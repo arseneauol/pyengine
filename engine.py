14bd1dc0913ce55ae9847fcb76d0b56fa24a435d
olivier arseneault <arseneauol@gmail.com>
2020-05-30 22:50:52 +0000

    basic addond

--- engine.py
+++ engine.py
@@ -1,9 +1,13 @@
 class GameObject:
-  def __init__(self,Transform,Rendereur,componentList):
-    self.transform = Transform
-    self.renderer = Rendereur
+  def __init__(self,transform,rendereur,componentList):
+    self.transform = transform
+    self.renderer = rendereur
     self.components = componentList
-
+  def update(self):
+    for component in self.components:
+      component.update()
+  def Render(self):
+    self.renderer.draw()
 class Vector2:
   def __init__(self,x,y):
     self.x = x
@@ -11,10 +15,22 @@ class Vector2:
 class Rotation:
   def __init__(self,angle):
     self.angle = angle
-
 class Transform:
   def __init(self,position,rotation,scale):
     self.position = Vector2(position)
     self.rotation = rotation
-    self.scale = scale
-
+    self.scale = Vector2(scale)
+class Renderer:
+  def __init__(self,sprite):
+    self.sprite =sprite
+  def draw(self):
+    sprite = 1 
+class Scene:
+  def __init__(self, lisOfgameObeject):
+    self.gameObjects = lisOfgameObeject
+  def update(self):
+    for gameObject in self.gameObjects:
+      gameObject.update()
+class Sprite:
+  def __init__(self,image):
+    self.image =image;
--- main.py
+++ main.py
@@ -2,9 +2,25 @@
 import pygame
 import engine
 pygame.init()
-screen = pygame.display.set_mode((240,180))
+
+screen = pygame.display.set_mode((600,400))
 running = True
 while running:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
+def loadScene():
+  global Scene
+  gameObjects = []
+
+  mtransform = engine.Transform(engine.Vector2(50,50))
+  spaceShip = engine.GameObject(mtransform,engine.Renderer())
+
+  gameObjects.append(spaceShip)
+
+
+
+  scene =  engine.Scene(gameObjects)
+  return
+def loadPreferane():
+  return
