c9c2de7a44342536286ef1b749a6c56a7b3e097e
arseneauol <arseneauol@penguin>
2020-05-30 21:16:26 -0400

    adding pycharm

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #! /usr/bin/python3.5
 import pygame
 import engine
+
 pygame.init()
-screen = pygame.display.set_mode((240,180))
+screen = pygame.display.set_mode((240, 180))
 running = True
 while running:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			running = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
